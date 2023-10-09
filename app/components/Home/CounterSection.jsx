a3895bd94dd3d0cb3bf95f238b13e24e0d42a9e1
naiemhosen430 <naiemhosen430@gmail.com>
2023-10-09 10:46:00 +0600

    d

--- app/components/Home/CounterSection.jsx
+++ app/components/Home/CounterSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function CounterSection() {
   return (
     <>
-      <div className="p-10 flex items-center justify-center space-x-5 space-y-5">
+      <div className="p-10 lg:flex lg:selection:items-center lg:selection:justify-center space-x-5 space-y-5">
         <div className="p-10 lg:w-3/12 text-center">
           <h1 className="text-5xl font-bold p-5">00</h1>
           <h1 className="text-xl">FINISHED SESSIONS</h1>
